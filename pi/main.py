c85372c0129d3ba8dd619747f2b0310eb34d2d5a
Sahil Kale <sahil.kale1@gmail.com>
2024-10-30 09:38:01 -0400

    Ball controller tweaks and cleanup

--- pi/main.py
+++ pi/main.py
@@ -9,6 +9,7 @@ import argparse
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 import time
+from point import Point
 
 import os, pty
 
@@ -26,8 +27,11 @@ class MainControlLoop:
         servo_offsets: list[float] = [0, 0, 0],  # degrees
         virtual: bool = False,
         run_visualizer: bool = False,
-        run_controller: bool = False,
+        run_controller: bool = True,
+        tune_controller: bool = False,
     ):
+        self.pause_period = 0.01
+        self.saturate_angle = 14.0
         self.params = {
             "lh": 41 / 1000,
             "la": 51 / 1000,
@@ -53,9 +57,11 @@ class MainControlLoop:
 
         self.pk = PlatformKinematicsModule(self.attachment_points)
         self.sk = ServoKinematicsModule(self.params["lh"], self.params["la"])
-        
+
         # The gains are just set arbitrarily for now
-        self.ball_controller = BallController(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+        self.ball_controller = BallController(
+            1.0, 1.0, 1.0, 1.0, 1.0, 1.0, self.pause_period, self.saturate_angle
+        )
 
         self.run_visualizer = run_visualizer
         self.run_controller = run_controller
@@ -73,31 +79,35 @@ class MainControlLoop:
 
         self.pc = PlatformController(serial_port, debug=False)
 
-        self.pause_period = 0.01
-
     def create_sliders(self):
         """Create sliders for pitch, roll, and height"""
         ax_pitch = plt.axes([0.15, 0.02, 0.65, 0.03], facecolor="lightgoldenrodyellow")
         ax_roll = plt.axes([0.15, 0.06, 0.65, 0.03], facecolor="lightgoldenrodyellow")
         ax_height = plt.axes([0.15, 0.10, 0.65, 0.03], facecolor="lightgoldenrodyellow")
 
-        self.slider_pitch = Slider(ax_pitch, "Pitch", -14.0, 14.0, valinit=0.0)
-        self.slider_roll = Slider(ax_roll, "Roll", -14.0, 14.0, valinit=0.0)
+        self.slider_pitch = Slider(
+            ax_pitch, "Pitch", -self.saturate_angle, self.saturate_angle, valinit=0.0
+        )
+        self.slider_roll = Slider(
+            ax_roll, "Roll", -self.saturate_angle, self.saturate_angle, valinit=0.0
+        )
         self.slider_height = Slider(ax_height, "Height", 0.02, 0.1, valinit=0.06)
 
     def run(self):
         while True:
             # Get pitch, roll, and height from the sliders
             if self.run_controller:
-                # TODO: integrate real control logic
                 # Get desired position from trajectory controller
                 # Get current position from CV controller (mocked out for now)
-                desired_position = [1, 1]
-                current_position = [2, 2]
-                output_angles = self.ball_controller.run_control_loop(desired_position, current_position)
-                
-                # (TODO) Pass the output angle to the IK controller
-                pass
+                desired_position = Point(1.0, 2.0)
+                current_position = Point(0.0, 0.0)
+                output_angles = self.ball_controller.run_control_loop(
+                    desired_position, current_position
+                )
+
+                pitch_rad = output_angles[0]
+                roll_rad = output_angles[1]
+                height = 0.060  # default height
             else:
                 pitch_rad = np.deg2rad(self.slider_pitch.val)
                 roll_rad = np.deg2rad(self.slider_roll.val)
@@ -145,7 +155,6 @@ if __name__ == "__main__":
         "--visualize", action="store_true", help="Run the 3D visualization"
     )
 
-    # add an argument for the port
     parser.add_argument(
         "--port",
         type=str,
@@ -153,20 +162,25 @@ if __name__ == "__main__":
         help="The serial port for the platform controller",
     )
 
-    # add an argument for virtual
     parser.add_argument(
         "--virtual",
         action="store_true",
         help="Run the control loop in virtual mode",
     )
 
-    args = parser.parse_args()
+    parser.add_argument(
+        "--run_controller",
+        action="store_true",
+        help="Run the control loop",
+    )
 
-    # for now, if visualize is not set, raise an error saying that the ball controller is not implemented yet and needs to run with the visualize flag
-    if not args.visualize:
-        raise NotImplementedError(
-            "Ball controller not implemented yet. Please run with the --visualize flag."
-        )
+    parser.add_argument(
+        "--tune_controller",
+        action="store_true",
+        help="Run the control loop in tuning mode",
+    )
+
+    args = parser.parse_args()
 
     # Current computed offsets are [0, 8, 10] for servo 0, 1, and 2 respectively
     servo_offsets = [0, 8, 10]
@@ -176,5 +190,6 @@ if __name__ == "__main__":
         virtual=args.virtual,
         run_visualizer=args.visualize,
         servo_offsets=servo_offsets,
+        tune_controller=args.tune_controller,
     )
     mcl.run()
