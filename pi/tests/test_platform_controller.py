a47b4947dda6b353919e74d25211eaca0d9dcf01
upadhyaydhruv <dmupadhyay12@gmail.com>
2024-11-17 10:42:55 -0500

    fixed unit tests

--- pi/tests/test_platform_controller.py
+++ pi/tests/test_platform_controller.py
@@ -37,18 +37,18 @@ def test_platform_controller_write_duty_cycles_encoded():
     fakeser = create_fake_serial()
 
     pc = platform_controller.PlatformController(fakeser[0])
-    pc.write_duty_cycles(1000, 2000, 3000)
+    pc.write_steps(50, 100, 150)
 
     bytes = bytearray()
-    dutycycle_1_bytes = (1000).to_bytes(2, byteorder="little", signed=False)
-    dutycycle_2_bytes = (2000).to_bytes(2, byteorder="little", signed=False)
-    dutycycle_3_bytes = (3000).to_bytes(2, byteorder="little", signed=False)
-    bytes.extend(dutycycle_1_bytes)
-    bytes.extend(dutycycle_2_bytes)
-    bytes.extend(dutycycle_3_bytes)
+    step_1_bytes = (50).to_bytes(2, byteorder="little", signed=False)
+    step_2_bytes = (100).to_bytes(2, byteorder="little", signed=False)
+    step_3_bytes = (150).to_bytes(2, byteorder="little", signed=False)
+    bytes.extend(step_1_bytes)
+    bytes.extend(step_2_bytes)
+    bytes.extend(step_3_bytes)
 
     # expect a checksum to be written, which is a uint32_t little endian sum of the 3 duty cycles
-    checksum = sum([1000, 2000, 3000])
+    checksum = sum([50, 100, 150])
     checksum_bytes = checksum.to_bytes(4, byteorder="little", signed=False)
     bytes.extend(checksum_bytes)
 
@@ -62,14 +62,14 @@ def test_duty_cycle_resolver_from_angle():
     fakeser = create_fake_serial()
     pc = platform_controller.PlatformController(fakeser[0])
 
-    angle_ninety_degrees_duty_cycle = pc.compute_duty_cycle_from_angle(np.pi / 2)
+    angle_ninety_degrees_step = pc.compute_steps_from_angle(np.pi / 2)
 
-    assert angle_ninety_degrees_duty_cycle == 1400
+    assert angle_ninety_degrees_step == 50
 
-    angle_zero_duty_cycle = pc.compute_duty_cycle_from_angle(0)
+    angle_zero_degrees_step = pc.compute_steps_from_angle(0)
 
-    assert angle_zero_duty_cycle == 500
+    assert angle_zero_degrees_step == 0
 
-    angle_full_duty_cycle = pc.compute_duty_cycle_from_angle(np.pi)
+    angle_full_duty_cycle = pc.compute_steps_from_angle(np.pi)
 
-    assert angle_full_duty_cycle == 2300
+    assert angle_full_duty_cycle == 100
